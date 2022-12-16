84f9d95db28ba385a0bacb4a5013b1a9b0818d49
goddetnalk <goddentalk@gmail.com>
2022-12-16 16:47:32 +0300

    Задание стэк

---
+++ Стек/Стек/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Стек
+{
+    public partial class Form1 : Form
+    {
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox2.Text);
+            stack = new MyStack<string>(n);
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+
+            textBox3.Text = Convert.ToString(stack.Count);
+            textBox4.Text = Convert.ToString(stack.Capacity);
+        }
+
+
+        MyStack<string> stack;
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (stack.IsNoFull())
+            {
+                stack.Push(textBox1.Text);
+                Visual();
+                textBox1.Clear();
+            }
+            else label1.Text = "Стек переполнен";
+            textBox1.Focus();
+            textBox3.Text = Convert.ToString(stack.Count);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (stack.IsEmpty())
+            {
+                label1.Text = "Стек пуст";
+            }
+            else
+            {
+                label2.Text = stack.Pop();
+                Visual();
+            }
+            textBox3.Text = Convert.ToString(stack.Count);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            label2.Text = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (stack.IsEmpty())
+            {
+                label1.Text = "Стек пуст";
+            }
+            else
+            {
+                label2.Text = stack.Top();
+                Visual();
+            }
+        }
+        private void Visual()
+        {
+            if (stack.size == 0)
+            {
+                dataGridView1.RowCount = 1;
+                dataGridView1.ColumnCount = 1;
+                dataGridView1.Rows[0].Cells[0].Value = "";
+            }
+            else
+            {
+                dataGridView1.RowCount = 1;
+                dataGridView1.ColumnCount = stack.size;
+            }
+            string[] s = stack.ToArray();
+            for (int i = 0; i < stack.size; i++)
+            {
+                dataGridView1.Rows[0].Cells[i].Value = s[i];
+            }
+
+
+        }
+
+    }
+}
---
+++ Стек/Стек/MyStack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Стек
+{
+    public class MyStack<T>
+    {
+        public int size = 0;
+        private T[] array;
+       
+         public MyStack(int n)
+        {
+            array = new T[n];
+        }
+
+        public bool Push(T value)
+        {
+            if (IsNoFull())
+            {
+                array[size] = value;
+                size++;
+                return true;
+            }
+            else return false;
+
+        }
+        public T Pop()
+        {
+            size--;
+
+            return array[size];
+
+
+        }
+
+        public bool IsNoFull()
+        {
+            if (size != this.array.Length)
+                return true;
+            else
+                return false;
+        }
+        public bool IsEmpty()
+        {
+            if (size == 0)
+                return true;
+            else return false;
+        }
+
+        public T Top()
+        {
+            return array[size-1];
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return size;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] new_array = new T[size];
+            for (int i = 0; i < new_array.Length; i++)
+            {
+                new_array[i] = array[i];
+            }
+            return new_array;
+        }
+    }
+}
